a86955f579b6691f21e0e5fb1e4fe1f9e21b2dba
aitxiaogang <aitxiaogang@163.com>
2017-07-29 20:01:28 +0800

    添加拦截器

---
+++ src/main/java/com/xiaogang/shop/web/inter/LoginInterceptor.java
@@ -0,0 +1,30 @@
+package com.xiaogang.shop.web.inter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class LoginInterceptor implements HandlerInterceptor{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		System.out.println("LoginInterceptor.preHandle()");
+		return false;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("LoginInterceptor.postHandle()");
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		System.out.println("LoginInterceptor.afterCompletion()");
+	}
+
+}
